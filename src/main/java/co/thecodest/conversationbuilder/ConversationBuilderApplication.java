015e7834d5df878d88e6f567d0216b15faaf316d
Sawicki Rafal <sawickirafal@outlook.com>
2022-05-25 15:02:26 +0100

    Init project

---
+++ src/main/java/co/thecodest/conversationbuilder/ConversationBuilderApplication.java
@@ -0,0 +1,13 @@
+package co.thecodest.conversationbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversationBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConversationBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/thecodest/conversationbuilder/ConversationBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package co.thecodest.conversationbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversationBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
