2cdedf543a9974ddcc9870951e09607453d6ce38
Sawicki Rafal <sawickirafal@outlook.com>
2022-05-26 09:14:27 +0100

    Update User Client to handle errors

--- src/main/java/co/thecodest/conversationbuilder/user/client/UserClient.java
+++ src/main/java/co/thecodest/conversationbuilder/user/client/UserClient.java
@@ -2,15 +2,19 @@ package co.thecodest.conversationbuilder.user.client;
 
 import co.thecodest.conversationbuilder.user.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class UserClient {
 
     @Value("${external.api.users.url}")
@@ -19,7 +23,16 @@ public class UserClient {
     private final RestTemplate restTemplate;
 
     public List<UserDTO> getAllUsers() {
-        UserDTO[] users = restTemplate.getForObject(usersUrl, UserDTO[].class);
-        return Arrays.asList(users);
+        List<UserDTO> users = new ArrayList<>();
+        try {
+            UserDTO[] remoteUsers = restTemplate.getForObject(usersUrl, UserDTO[].class);
+            if (remoteUsers != null) {
+                users.addAll(Arrays.asList(remoteUsers));
+            }
+        } catch (RestClientException e) {
+            log.error("User Rest Api call failure: Reason: \n" + e);
+        }
+        return users;
     }
+
 }
--- src/test/java/co/thecodest/conversationbuilder/user/client/UserClientTest.java
+++ src/test/java/co/thecodest/conversationbuilder/user/client/UserClientTest.java
@@ -1,20 +1,29 @@
 package co.thecodest.conversationbuilder.user.client;
 
 import co.thecodest.conversationbuilder.user.dto.UserDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.ResponseCreator;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @RestClientTest(UserClient.class)
@@ -22,18 +31,18 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
 @TestPropertySource(properties = {"external.api.users.url=/slack/users"})
 class UserClientTest {
 
-    @Autowired
-    private UserClient userClient;
+    public static final String USERS_URL = "/slack/users";
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private UserClient userClient;
 
     @Autowired
     private MockRestServiceServer mockRestServiceServer;
 
     @Test
     void userClientSuccessfullyReturnsUsers() throws Exception {
-
         final String teamId = "TEAMIDXX001";
         final UserDTO user_1 = new UserDTO("USERIDXX001", teamId, "USER001");
         final UserDTO user_2 = new UserDTO("USERIDXX002", teamId, "USER002");
@@ -44,23 +53,44 @@ class UserClientTest {
         users.add(user_2);
         users.add(user_3);
 
-        String usersJson = this.objectMapper
-                .writeValueAsString(users);
+        final String usersJson = objectMapper.writeValueAsString(users);
 
         this.mockRestServiceServer
-                .expect(requestTo("/slack/users"))
+                .expect(requestTo(USERS_URL))
                 .andRespond(withSuccess(usersJson, MediaType.APPLICATION_JSON));
 
         final List<UserDTO> actualUsers = userClient.getAllUsers();
 
         assertThat(actualUsers.size()).isEqualTo(users.size());
 
-        users.forEach(user->{
+        users.forEach(user -> {
             UserDTO actualUser = actualUsers.stream().filter(au -> au.getId().equals(user.getId())).findAny().get();
             assertThat(actualUser.getName()).isEqualTo(user.getName());
             assertThat(actualUser.getTeamId()).isEqualTo(user.getTeamId());
         });
-
     }
 
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForUserServiceSuccessfullyReturnsEmptyList")
+    void userServiceSuccessfullyReturnsEmptyList(ResponseCreator remoteCallResponseCreator) {
+        this.mockRestServiceServer
+                .expect(requestTo(USERS_URL))
+                .andRespond(remoteCallResponseCreator);
+
+        final List<UserDTO> actualUsers = userClient.getAllUsers();
+        assertThat(actualUsers).isEmpty();
+    }
+
+    private static Stream<Arguments> provideArgumentsForUserServiceSuccessfullyReturnsEmptyList()
+            throws JsonProcessingException {
+        final String nullUsersList = objectMapper.writeValueAsString(null);
+        final String emptyUsersList = objectMapper.writeValueAsString(Collections.emptyList());
+
+        return Stream.of(
+                Arguments.of(withSuccess(nullUsersList, MediaType.APPLICATION_JSON)),
+                Arguments.of(withSuccess(emptyUsersList, MediaType.APPLICATION_JSON)),
+                Arguments.of(withStatus(HttpStatus.BAD_REQUEST)),
+                Arguments.of(withStatus(HttpStatus.INTERNAL_SERVER_ERROR))
+        );
+    }
 }
