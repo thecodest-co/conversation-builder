1c3478dbbacd55632e9ff283c8d89b250245ad08
Sawicki Rafal <sawickirafal@outlook.com>
2022-06-02 12:00:55 +0200

    Add api key authorization

--- src/main/java/co/thecodest/conversationbuilder/external/config/RestTemplateConfiguration.java
+++ src/main/java/co/thecodest/conversationbuilder/external/config/RestTemplateConfiguration.java
@@ -1,5 +1,6 @@
 package co.thecodest.conversationbuilder.external.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,22 +13,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class RestTemplateConfiguration {
 
     @Value("${external.service.slack.bot.base.url}")
     private String slackBotBaseUrl;
 
+    private final SlackRestTemplateHeaderModifierInterceptor interceptor;
+
     @Bean
     public RestTemplate slackBotApiRestTemplate(RestTemplateBuilder restTemplateBuilder) {
-        RestTemplate restTemplate = restTemplateBuilder.rootUri(slackBotBaseUrl).build();
-        List<ClientHttpRequestInterceptor> interceptors
-                = restTemplate.getInterceptors();
+        final RestTemplate restTemplate = restTemplateBuilder.rootUri(slackBotBaseUrl).build();
+        addInterceptor(restTemplate);
+        return restTemplate;
+    }
+
+    private void addInterceptor(RestTemplate restTemplate) {
+        List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
         if (CollectionUtils.isEmpty(interceptors)) {
             interceptors = new ArrayList<>();
         }
-        interceptors.add(new RestTemplateHeaderModifierInterceptor());
+        interceptors.add(interceptor);
         restTemplate.setInterceptors(interceptors);
-        return restTemplate;
     }
 
 }
